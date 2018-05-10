376d0276c50382c6b6184f80c16504c620bf0272
jailtonurbano <jailton.michele@gmail.com>
2018-05-10 01:53:35 +0000

    Adding api version

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-require 'api_version_constrainst'
+require 'api_version_constraint'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
