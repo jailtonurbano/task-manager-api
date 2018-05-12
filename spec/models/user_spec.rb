817aec4ccb3a65f75c93c00de6be7b6be9348c5a
jailtonurbano <jailton.michele@gmail.com>
2018-05-12 01:08:44 +0000

    Melhorias 1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe User, type: :model do
   before { @user = FactoryBot.build(:user) }
 
   it { expect(@user).to respond_to(:email) }
+  it { expect(@user).to respond_to(:name) }
+  it { expect(@user).to respond_to(:password) }
+  it { expect(@user).to respond_to(:password_confirmation) }
+  it { expect(@user).to. be_valid }
 end
