e27296ac136d2ecb49b6478dc46cccfd5de72596
jailtonurbano <jailton.michele@gmail.com>
2018-05-15 15:32:37 +0000

    Changing double quotes to simple quotes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe User, type: :model do
   let(:user) { build(:user) }
   it { expect(user).to respond_to(:email) }
 
-  #context "when name is blank" do
-  #  before { user.name = " "}
+  #context 'when name is blank' do
+  #  before { user.name = ' '}
 
   #  it { expect(user).not_to be_valid }
 
   #end
 
-  #context "when name is nil" do
+  #context 'when name is nil' do
 #    before { user.name = nil}
 
  #   it { expect(user).not_to be_valid }
@@ -22,7 +22,7 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:email)}
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive}
   it { is_expected.to validate_confirmation_of(:password)}
-  it { is_expected.to allow_value("jailton.michele@gmail.com").for(:email)}
+  it { is_expected.to allow_value('jailton.michele@gmail.com').for(:email)}
   #it { is_expected.to validate_numericality_of(:age)}
   #subject  { @user = build(:user) }
   #before { @user = FactoryBot.build(:user) }
