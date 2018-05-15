f1e6590c683ac43a453f6166c5a83f187dc3d65c
jailtonurbano <jailton.michele@gmail.com>
2018-05-15 00:52:55 +0000

    Addind shoulda matcher for user specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
     factory :user do
         email { Faker::Internet.email }
         password "123456"
-        password_confirmation "123456"
+        password_confirmation "123456" 
     end
 end
