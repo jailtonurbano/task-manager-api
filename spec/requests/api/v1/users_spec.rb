6604ca136229c6a81f68c878b90d5729114c872b
jailtonurbano <jailton.michele@gmail.com>
2018-05-22 15:12:19 +0000

    Adding action update with testes

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe 'Users API', type: :request do
     end
 
     describe 'POST /users' do 
-        before do
+                before do
             headers = { 'Accept' => 'application/vnd.task-manager.v1'}
             post '/users', params: { user: user_params }, headers: headers
         end 
@@ -55,6 +55,39 @@ RSpec.describe 'Users API', type: :request do
             let(:user_params) { attributes_for(:user, email: 'invalid@')}
 
             it 'returns status code 422' do
+                expect(response).to have_http_status(422)
+            end
+
+            it 'returns the json data for the errors' do
+                user_response = JSON.parse(response.body, symbolize_names: true)
+                expect(user_response).to have_key(:errors)
+            end
+        end
+    end
+
+    describe 'PUT /users/:id' do
+        before do
+            headers = { 'Accept' => 'application/vnd.task-manager.v1'}
+            put "/users/#{user_id}", params: { user: user_params }, headers: headers
+        end
+        context 'when the request params are valid' do
+            let(:user_params) { {email: "newtom512@email.com"} }
+            
+            it 'returns status code 200' do
+                expect(response).to have_http_status(200)
+            end
+
+            it 'return the json data for the updated user' do
+                user_response = JSON.parse(response.body, symbolize_names: true)
+                expect(user_response[:email]).to eq(user_params[:email])
+            end
+        end
+
+
+        context 'when the request params are invalid' do
+            let(:user_params) { {email: "invalidemail@"} }
+            
+            it 'returns status code 200' do
                 expect(response).to have_http_status(422)
             end
 
