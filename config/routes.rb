923e1db4e008aa3838fa94816cc642abdaa61e77
jailtonurbano <jailton.michele@gmail.com>
2018-06-13 16:30:42 +0000

    Adding Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, constrainst: { subdomain: 'api' }, path: '/' do
     namespace :v1, path: '/', constrainst: ApiVersionConstraint.new(version: 1 , default: true) do
       resources :users, only: [:show, :create, :update, :destroy]
+      resources :sessions, only: [:create]
     end
   end
 end
